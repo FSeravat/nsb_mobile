37943425d45938060621fe6c802305a62dcd3af8
JeuMelhor <jessica.melhor@outlook.com>
2022-04-10 20:24:49 -0300

    Feat: Create UpdateScreen

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
+import UpdateProfile from './screens/UpdateProfile';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="UpdateProfile" component={UpdateProfile}/>
         <Stack.Screen name="Login" component={Login}/>
       </Stack.Navigator>
     </NavigationContainer>
