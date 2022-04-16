730e01b52a8fdc881e4d43732a27b987d4b5f8e0
neomugiwara <felipecostatavares@gmail.com>
2022-04-15 23:38:53 -0300

    Update Screen 'UpdateProfile'

--- App.tsx
+++ App.tsx
@@ -11,9 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name="CreateNewAccount" component={CreateNewAccount}/>
-        {/* <Stack.Screen name="UpdateProfile" component={UpdateProfile}/>
-        <Stack.Screen name="Login" component={Login}/> */}
+        {/* <Stack.Screen name="CreateNewAccount" component={CreateNewAccount}/> */}
+        <Stack.Screen name="UpdateProfile" component={UpdateProfile}/>
+        {/* <Stack.Screen name="Login" component={Login}/> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/UpdateProfile/styles.ts
+++ src/screens/UpdateProfile/styles.ts
@@ -4,37 +4,12 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
-        alignItems: 'center',
+        alignItems: 'stretch',
         justifyContent: 'center',
         fontWeight:'bold',
         padding:25
     },
-
-    input:{
-        padding:5,
-        borderStyle:'solid',
-        borderWidth:2,
-        borderRadius:5,
-        borderColor:'#C4CED6'
-    },
-
-    elementsContainer:{
-        padding:10,
-        width:'100%'
-    },
-
-    mainButton:{
-        backgroundColor:'#D3455B'
-    },
-
-    secundaryButton:{
-        borderColor:'#EEBCC3',
-        borderStyle:'solid',
-        borderWidth:2,
-        borderRadius:5
-    },
-
-    fieldTitle:{
-        textAlign:'left'
+    contentContainer: {
+        paddingVertical: 20
     }
   })
