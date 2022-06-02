4fda26a2dc551bcda1146972c34f35ce30db0b1e
FSeravat <felipecostatavares@gmail.com>
2022-06-02 01:16:03 -0300

    New: Auth and Hooks

--- App.tsx
+++ App.tsx
@@ -1,35 +1,11 @@
 import React from "react";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./src/screens/Login";
-import UpdateProfile from "./src/screens/UpdateProfile";
-import CreateNewAccount from "./src/screens/CreateNewAccount";
-import SliderIntro from "./src/screens/SliderIntro";
-import DonateRequest from "./src/screens/DonateRequest";
-import Main from "./src/screens/Main";
-import Location from "./src/screens/Location";
-import Requests from "./src/screens/Requests";
-import Notification from "./src/screens/Notification";
-
-const Stack = createNativeStackNavigator();
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{ headerShown: false }}
-        initialRouteName="SliderIntro"
-      >
-        <Stack.Screen name="SliderIntro" component={SliderIntro} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="CreateNewAccount" component={CreateNewAccount} />
-        <Stack.Screen name="UpdateProfile" component={UpdateProfile} />
-        <Stack.Screen name="DonateRequest" component={DonateRequest} />
-        <Stack.Screen name="Location" component={Location} />
-        <Stack.Screen name="Requests" component={Requests} />
-        <Stack.Screen name="Notification" component={Notification} />
-      </Stack.Navigator>
+      <Routes />
     </NavigationContainer>
   );
 }
